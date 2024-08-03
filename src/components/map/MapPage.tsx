d77981bbbec249a60e8d83772dc340ef0a9682fa
VishReddy <vishwareddy@gmail.com>
2024-08-03 15:24:35 -0400

    feat: mapbox

--- src/components/map/MapPage.tsx
+++ src/components/map/MapPage.tsx
@@ -1,19 +1,39 @@
-// ** Next Imports
-import React from 'react'
+// ** React Imports
+import { useRef } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
 
-// ** Theme IMports
+// ** Theme Imports
 import { useTheme } from '@mui/material/styles'
 
+// ** Map Imports
+import Map, { NavigationControl } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 const MapPage = () => {
-  
-    const theme = useTheme()
+  const theme = useTheme()
+  const mapRef = useRef<any>(null)
+  const mapBoxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!
 
   return (
-    <Box className={`h-full w-full rounded-lg flex flex-col items-center text-center justify-between shadow-lg`} sx={{backgroundColor: theme.palette.background.paper}}>
-        
+    <Box 
+      className={`h-full w-full rounded-lg flex flex-col items-center text-center justify-between shadow-lg scale-100`} 
+      sx={{ backgroundColor: theme.palette.background.paper }}
+    >
+      <Map
+        ref={mapRef}
+        mapboxAccessToken={mapBoxToken}
+        initialViewState={{
+          longitude: -74.5,
+          latitude: 40,
+          zoom: 9
+        }}
+        style={{width: '100%', height: '100%', borderRadius: '8px'}}
+        mapStyle={"mapbox://styles/mapbox/dark-v11"}
+      >
+      </Map>
+     {/* TODO: No borders, transparent vingette around map */}
     </Box>
   )
 }
