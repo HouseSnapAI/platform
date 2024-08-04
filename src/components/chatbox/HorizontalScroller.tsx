2485841ae97748190006ce7cd6a005cc56b0bf60
VishReddy <vishwareddy@gmail.com>
2024-08-04 18:39:10 -0400

    dep issues

--- src/components/chatbox/HorizontalScroller.tsx
+++ src/components/chatbox/HorizontalScroller.tsx
@@ -20,59 +20,59 @@ type HorizontalScrollerProps = {
 }
 
 const HorizontalScroller = ({setInputValue, email, userInfo, setUserInfo }: HorizontalScrollerProps) => {
-  return (
-    <Box className='w-[64%] flex flex-col justify-center items-center gap-2'>
-      {/* EXAMPLE LISTINGS */}
-      <Box className="w-full flex flex-row relative listings gap-2">
-        <Box className="h-full flex listings-slide gap-2">
-          {exampleListings.map((listing, index) => (
-            <Listing key={index} listing={listing} email={email} userInfo={userInfo} setUserInfo={setUserInfo} />
-          ))}
-        </Box>
-        <Box className="h-full flex listings-slide gap-2">
-          {exampleListings.map((listing, index) => (
-            <Listing key={index} listing={listing} email={email} userInfo={userInfo} setUserInfo={setUserInfo} />
-          ))}
-        </Box>
-      </Box>
+  // return (
+  //   <Box className='w-[64%] flex flex-col justify-center items-center gap-2'>
+  //     {/* EXAMPLE LISTINGS */}
+  //     <Box className="w-full flex flex-row relative listings gap-2">
+  //       <Box className="h-full flex listings-slide gap-2">
+  //         {exampleListings.map((listing, index) => (
+  //           <Listing key={index} listing={listing} email={email} userInfo={userInfo} setUserInfo={setUserInfo} />
+  //         ))}
+  //       </Box>
+  //       <Box className="h-full flex listings-slide gap-2">
+  //         {exampleListings.map((listing, index) => (
+  //           <Listing key={index} listing={listing} email={email} userInfo={userInfo} setUserInfo={setUserInfo} />
+  //         ))}
+  //       </Box>
+  //     </Box>
 
-      {/* EXAMPLE PROMPT ONE */}
-      <Box className="w-full flex flex-row relative listings gap-2">
-        <Box className="h-full flex listings-slide-reverse gap-2">
-          {examplePromptsOne.map((prompt, index) => (
-            <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
-              <Typography variant='subtitle2'>{prompt}</Typography>
-            </Box>
-          ))}
-        </Box>
-        <Box className="h-full flex listings-slide-reverse gap-2">
-          {examplePromptsOne.map((prompt, index) => (
-            <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
-              <Typography variant='subtitle2'>{prompt}</Typography>
-            </Box>
-          ))}
-        </Box>
-      </Box>
+  //     {/* EXAMPLE PROMPT ONE */}
+  //     <Box className="w-full flex flex-row relative listings gap-2">
+  //       <Box className="h-full flex listings-slide-reverse gap-2">
+  //         {examplePromptsOne.map((prompt, index) => (
+  //           <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
+  //             <Typography variant='subtitle2'>{prompt}</Typography>
+  //           </Box>
+  //         ))}
+  //       </Box>
+  //       <Box className="h-full flex listings-slide-reverse gap-2">
+  //         {examplePromptsOne.map((prompt, index) => (
+  //           <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
+  //             <Typography variant='subtitle2'>{prompt}</Typography>
+  //           </Box>
+  //         ))}
+  //       </Box>
+  //     </Box>
 
-      {/* EXAMPLE PROMPT 2 */}
-      <Box className="w-full flex flex-row relative listings gap-2">
-        <Box className="h-full flex listings-slide gap-2">
-          {examplePromptsTwo.map((prompt, index) => (
-            <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
-              <Typography variant='subtitle2'>{prompt}</Typography>
-            </Box>
-          ))}
-        </Box>
-        <Box className="h-full flex listings-slide gap-2">
-          {examplePromptsTwo.map((prompt, index) => (
-            <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
-              <Typography variant='subtitle2'>{prompt}</Typography>
-            </Box>
-          ))}
-        </Box>
-      </Box>
-    </Box>
-  );
+  //     {/* EXAMPLE PROMPT 2 */}
+  //     <Box className="w-full flex flex-row relative listings gap-2">
+  //       <Box className="h-full flex listings-slide gap-2">
+  //         {examplePromptsTwo.map((prompt, index) => (
+  //           <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
+  //             <Typography variant='subtitle2'>{prompt}</Typography>
+  //           </Box>
+  //         ))}
+  //       </Box>
+  //       <Box className="h-full flex listings-slide gap-2">
+  //         {examplePromptsTwo.map((prompt, index) => (
+  //           <Box key={index} onClick={() => setInputValue(prompt)} className='w-full h-full px-4 py-3 border-[1px] border-[#393939] text-white/70 hover:text-white rounded-sm hover:cursor-pointer ease-in-out duration-300 hover:bg-[#4a4a4a]/30'>
+  //             <Typography variant='subtitle2'>{prompt}</Typography>
+  //           </Box>
+  //         ))}
+  //       </Box>
+  //     </Box>
+  //   </Box>
+  return(<></>);
 };
 
 export default HorizontalScroller;
