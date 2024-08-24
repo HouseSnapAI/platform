ce2b00a7b281233c266f8121fe0743cedf0897ee
avwarrier <avwarrier@ucdavis.edu>
2024-08-24 11:32:25 -0700

    work done

--- src/components/reports/sections/SafetyPage.tsx
+++ src/components/reports/sections/SafetyPage.tsx
@@ -104,6 +104,117 @@ const SafetyPage = ({crimeData, data, listing}: Props) => {
         }
     })
 
+    const ethnicityLabels = ["Hispanic", "Not Hispanic", "Unknown"];
+    const ethnicityValuesOffender = [
+        crimeData?.offender_ethnicity[0].value,
+        crimeData?.offender_ethnicity[1].value,
+        crimeData?.offender_ethnicity[2].value,
+    ];
+    const ethnicityValuesVictim = [
+        crimeData?.victim_ethnicity[0].value,
+        crimeData?.victim_ethnicity[1].value,
+        crimeData?.victim_ethnicity[2].value,
+    ];
+
+    const raceLabels = ["White", "Black or African American", "Unknown", "Asian", "Native Hawaiian", "American Indian or Alaskan Native"];
+    const raceValuesOffender = [
+        crimeData?.offender_race[0].value,
+        crimeData?.offender_race[1].value,
+        crimeData?.offender_race[2].value,
+        crimeData?.offender_race[3].value,
+        crimeData?.offender_race[4].value,
+        crimeData?.offender_race[5].value,
+    ];
+    const raceValuesVictim = [
+        crimeData?.victim_race[0].value,
+        crimeData?.victim_race[1].value,
+        crimeData?.victim_race[2].value,
+        crimeData?.victim_race[3].value,
+        crimeData?.victim_race[4].value,
+        crimeData?.victim_race[5].value,
+    ];
+
+    const sexLabels = ["Male", "Female", "Unknown"];
+    let totalOffender = crimeData?.offender_sex[0].Male + crimeData?.offender_sex[0].Female + crimeData?.offender_sex[0].Unknown;
+    let totalVictim = crimeData?.victim_sex[0].Male + crimeData?.victim_sex[0].Female + crimeData?.victim_sex[0].Unknown;
+    const sexValuesOffender = [
+        crimeData?.offender_sex[0].Male / totalOffender,
+        crimeData?.offender_sex[0].Female / totalOffender,
+        crimeData?.offender_sex[0].Unknown / totalOffender,
+    ];
+    const sexValuesVictim = [
+        crimeData?.victim_sex[0].Male / totalVictim,
+        crimeData?.victim_sex[0].Female / totalVictim,
+        crimeData?.victim_sex[0].Unknown / totalVictim,
+    ];
+
+    console.log(sexValuesOffender)
+    console.log(sexValuesVictim)
+
+    const sexBarData = {
+        labels: sexLabels,
+        datasets: [
+            {
+                label: 'Offender',
+                data: sexValuesOffender,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                barThickness: 15,
+            },
+            {
+                label: 'Victim',
+                data: sexValuesVictim,
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1,
+                barThickness: 15,
+            },
+        ],
+    
+    }
+
+    const ethnicityData = {
+        labels: ethnicityLabels,
+        datasets: [
+          {
+            label: "Offender",
+            data: ethnicityValuesOffender,
+            backgroundColor: ["#299ce3", "#116599", "#6b99b5"],
+            borderColor: "#121212"
+          },
+          {
+            label: "Victim",
+            data: ethnicityValuesVictim,
+            backgroundColor: ["#f5a940", "#b07320", "#e3c091"],
+            borderColor: "#121212"
+          },
+        ],
+      }
+    
+      const ethnicityChartOptions = {
+        cutout: '55%', // Adjust cutout to make the chart smaller,
+        aspectRatio: 1,
+        plugins: {
+          tooltip: { 
+            enabled: true,
+            callbacks: {
+                label: function(context: any) {
+                    let label = context.label,
+                      currentValue = context.raw,
+                      total = context.chart._metasets[context.datasetIndex].total;
+        
+                    let percentage = parseFloat((currentValue/total*100).toFixed(1));
+        
+                  return context.dataset.label + ": " + percentage + '%';
+                }
+            } 
+        },
+          legend: { display: false },
+        },
+        
+      };
+
     /**
      * Generates a data object for a bar chart that displays offender and victim age data.
      * 
@@ -155,11 +266,77 @@ const SafetyPage = ({crimeData, data, listing}: Props) => {
         },
         plugins: {
             legend: {
-                display: true, // Disable the legend
+                display: true,
                 position: 'top'
             },
         },
     };
+
+    const sexBarOptions: any = {
+        indexAxis: 'y',
+        scales: {
+            y: {
+                beginAtZero: true,
+                grid: {
+                    color: theme.palette.divider, // Set grid lines to white
+                    z: -1, // Ensure grid lines are behind the bars
+                },
+                
+            },
+            x: {
+                grid: {
+                    color: theme.palette.divider, // Set grid lines to white
+                    z: -1, // Ensure grid lines are behind the bars
+                },
+                ticks: {
+                    format: {
+                        style: 'percent'
+                    }
+                },
+            },
+        },
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top'
+            },
+        },
+    };
+    
+      const raceOptions = {
+        cutout: '0%', // Adjust cutout to make the chart smaller,
+        aspectRatio: 1,
+        plugins: {
+          tooltip: { 
+            enabled: true,
+            callbacks: {
+                label: function(context: any) {
+                    let label = context.label,
+                      currentValue = context.raw,
+                      total = context.chart._metasets[context.datasetIndex].total;
+        
+                    let percentage = parseFloat((currentValue/total*100).toFixed(1));
+        
+                  return percentage + '%';
+                }
+            } 
+        },
+          legend: { display: false },
+        },
+        
+      };
+
+    const raceData = (labels: any, values: any) => ({
+    type: 'doughnut',
+    labels: labels,
+    datasets: [
+        {
+        data: values,
+        backgroundColor: ['#0a99ff', '#f2be55', '#4ccf7a', '#9e6fd1', "#f76ac8", "#f27c83"],
+        borderColor: "#121212"
+        },
+    ],
+    })
 
     const typeData = (labels: any, values: any) => ({
         type: 'doughnut',
@@ -217,7 +394,7 @@ const SafetyPage = ({crimeData, data, listing}: Props) => {
                         ))
                     }
                 </Box>
-                <Box className='flex justify-center items-center gap-[7%] mt-[20px] flex-wrap w-[100%]'>
+                <Box className='flex justify-around items-center  mt-[40px] flex-wrap w-[100%]'>
                     <Box className='flex bg-[#181818] rounded-md shadow-lg flex-col justify-center items-center min-w-[150px] w-[27%] py-[15px] gap-[10px]'>
                         <Box className='h-[150px] w-[130px]  flex justify-center items-center'>
                             <Doughnut data={typeData(offenseTypeLabels, offenseTypeValues)} options={chartOptions} />
@@ -241,6 +418,54 @@ const SafetyPage = ({crimeData, data, listing}: Props) => {
                 <Box className='mt-[20px] flex items-start justify-center w-[100%] flex-col gap-[10px]'>
                     <Typography className='text-[#ababab] text-[16px]'>Age Data</Typography>
                     <Bar style={{width: "90%", height: "90%"}} data={ageBarData} options={ageBarOptions} />
+                </Box>
+
+                <Box className="mt-[40px] flex items-center justify-around flex-wrap">
+                    <Box className='flex flex-col items-center justify-center gap-[5px]'>
+                        <Typography className='text-[#ababab] text-[16px]'>Ethnicity Data</Typography>
+                        <Typography className='text-[#838383] text-[14px] mb-[5px]'>Hispanic, Not Hispanic, Unkown</Typography>
+                        <Box className='h-[220px] w-[220px]  flex justify-center items-center'>
+                            <Doughnut data={ethnicityData} options={ethnicityChartOptions} />
+                        T</Box>
+                    </Box>
+                    <Box className='flex justify-center items-center gap-[40px] bg-[#181818] rounded-sm shadow-lg h-[240px] px-[20px]'>
+                        <Box className='flex flex-col justify-center gap-[10px] items-center'>
+                            <Typography className='text-[#ababab] text-[16px]'>Offender Race</Typography>
+                            <Box className='h-[150px] w-[130px]  flex justify-center items-center'>
+                                <Doughnut data={raceData(raceLabels, raceValuesOffender)} options={raceOptions} />
+                            </Box>
+                            <Typography className='text-[#838383] text-[14px]'>Majority: {raceLabels[raceValuesOffender.indexOf(Math.max(...raceValuesOffender))]} - {Math.max(...raceValuesOffender)} crimes</Typography>
+                        </Box>
+                        <Box className='flex flex-col justify-center gap-[10px] items-center'>
+                            <Typography className='text-[#ababab] text-[16px]'>Victim Race</Typography>
+                            <Box className='h-[150px] w-[130px]  flex justify-center items-center'>
+                                <Doughnut data={raceData(raceLabels, raceValuesVictim)} options={raceOptions} />
+                            </Box>
+                            <Typography className='text-[#838383] text-[14px]'>{raceLabels[raceValuesVictim.indexOf(Math.max(...raceValuesVictim))]} - {Math.max(...raceValuesVictim)} victims</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+
+                <Box className='mt-[40px] flex justify-center gap-[20px] items-center'>
+                    <Box className='flex items-start justify-center w-[50%] flex-col gap-[10px]'>
+                        <Typography className='text-[#ababab] text-[16px]'>Sex Data</Typography>
+                        <Bar style={{width: "90%", height: "90%"}} data={sexBarData} options={sexBarOptions} />
+                    </Box>
+
+                    <Box className='bg-[#181818] rounded-lg shadow-md gap-[30px] flex justify-center items-center py-[20px] px-[20px]'>
+                        <Box className='flex flex-col justify-center gap-[10px]'>
+                            <Typography className='text-[#ababab] text-[16px] mb-[10px] underline'>Offender</Typography>
+                            <Typography className='text-[#838383] text-[14px]'>Male: {crimeData?.offender_sex[0].Male} crimes</Typography>
+                            <Typography className='text-[#838383] text-[14px]'>Female: {crimeData?.offender_sex[0].Female} crimes</Typography>
+                            <Typography className='text-[#838383] text-[14px]'>Unknown: {crimeData?.offender_sex[0].Unknown} crimes</Typography>
+                        </Box>
+                        <Box className='flex flex-col justify-center gap-[10px]'>
+                            <Typography className='text-[#ababab] text-[16px] mb-[10px] underline'>Victim</Typography>
+                            <Typography className='text-[#838383] text-[14px]'>Male: {crimeData?.victim_sex[0].Male} victims</Typography>
+                            <Typography className='text-[#838383] text-[14px]'>Female: {crimeData?.victim_sex[0].Female} victims</Typography>
+                            <Typography className='text-[#838383] text-[14px]'>Unknown: {crimeData?.victim_sex[0].Unknown} victims</Typography>
+                        </Box>
+                    </Box>
                 </Box>
                 
             </Box>
