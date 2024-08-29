3953e62c0338c02c7ed0a58af533facf154e8a1f
avwarrier <avwarrier@ucdavis.edu>
2024-08-29 00:44:51 -0700

    Environments basically done

--- src/components/reports/sections/SafetyPage.tsx
+++ src/components/reports/sections/SafetyPage.tsx
@@ -491,12 +491,14 @@ const SafetyPage = ({envData, crimeData, data, listing}: Props) => {
     const [envDataProcessed, setEnvDataProcessed] = useState<any>({});
     const [county, setCounty] = useState<String>("");
     const [envScore, setEnvScore] = useState<number>();
+    const [overallRisk, setOverallRisk] = useState<any>({});
 
     useEffect(() => {
         if(envData != undefined) {
             console.log(envData);
         
             let temp: any = {};
+            let riskVals: any = {}
             Object.keys(envData).map((key) => {
                 if (key == "county") {
                     setCounty(envData[key]);
@@ -505,25 +507,30 @@ const SafetyPage = ({envData, crimeData, data, listing}: Props) => {
                 } else {
                     let split = key.split("_");
                     let title = split[0];
-                    split.shift();
-                    let subType = split[split.length - 1]
-                    split.pop();
+                    if(title == "risk") {
+                        console.log(split)
+                        riskVals[split[1]] = envData[key];
+                    } else {
+                        split.shift();
+                        let subType = split[split.length - 1]
+                        split.pop();
 
-                    if (!(title in temp)) {
-                        temp[title] = {}
+                        if (!(title in temp)) {
+                            temp[title] = {}
+                        }
+
+                        let subTitle = split.join("_")
+
+                        if (!(subTitle in temp[title])) {
+                            temp[title][subTitle] = {}
+                        }
+
+                        temp[title][subTitle][subType] = envData[key];
                     }
-
-                    let subTitle = split.join("_")
-
-                    if (!(subTitle in temp[title])) {
-                        temp[title][subTitle] = {}
-                    }
-
-                    temp[title][subTitle][subType] = envData[key];
                 }
             })
-
-            console.log(temp)
+            
+            setOverallRisk(riskVals);
             setEnvDataProcessed(temp);
         }
         
@@ -653,8 +660,8 @@ const SafetyPage = ({envData, crimeData, data, listing}: Props) => {
                 </Box>
                 
             </Box>
-            <Box className='flex flex-col bg-[#121212] w-[47%] rounded-md py-[20px] px-[20px] items-start'>
-                <Box className="flex justify-between items-center w-full mb-[80px]">
+            <Box className='flex flex-col bg-[#121212] w-[47%] rounded-md py-[20px] px-[20px] items-start overflow-auto pb-[100px]'>
+                <Box className="flex justify-between items-center w-full">
                     <Typography className='text-[#e4e4e4] text-[22px]'>Environmental Score: {envScore}</Typography>
 
                     <Box className='flex justify-center items-center bg-[#313131] rounded-sm px-[10px] py-[2px]'>
@@ -662,13 +669,20 @@ const SafetyPage = ({envData, crimeData, data, listing}: Props) => {
                     </Box>
                 </Box>
 
-                {
-                    Object.keys(envDataProcessed).map((key: any, index: number) => {
-                        return (
-                            <EnvCard envData={envDataProcessed[key]} key={index}/>
-                        )
-                    })
-                }
+                <Box className="flex justify-center items-center gap-[10px] mt-[30px] mb-[50px]">
+                    <Typography className='text-[#ababab] text-[18px]'>Overall Risk - {overallRisk.rating}</Typography>
+                    <Typography className={`text-[#ababab] text-[18px] ml-[10px] ${overallRisk.score >= 70 ? 'text-[#db5a57]' : overallRisk.score >= 30 ? 'text-[#dfa137]' : 'text-[#349142]'}`}>{overallRisk.score != undefined && overallRisk.score.toFixed(2)}/100</Typography>
+                </Box>
+
+                <Box className="w-full  flex flex-col items-center gap-[30px]">
+                    {
+                        Object.keys(envDataProcessed).map((key: any, index: number) => {
+                            return (
+                                <EnvCard title={key} envData={envDataProcessed[key]} key={index}/>
+                            )
+                        })
+                    }
+                </Box>
             </Box>
         </Box>
     )
